7542700c6c2903ff554c62245ca8bbc735379745
edotm <edokyactus@gmail.com>
2024-02-07 22:39:58 +0100

    feat: change main page look

--- frontend/src/pages/JoinRoomPage.tsx
+++ frontend/src/pages/JoinRoomPage.tsx
@@ -1,31 +1,39 @@
 import { useState } from "react";
-import {BsPlus} from "react-icons/bs";
 
 function JoinRoomPage() {
   const [roomCode, setRoomCode] = useState<string>();
 
   return (
     <div className={"container-fluid pt-5"}>
-      <h2 className={"text-center"}>Join Room</h2>
-      <div className={"input-group"}>
-        <input
-          type="text"
-          className={"form-control"}
-          value={roomCode}
-          onInput={(e) => setRoomCode(e.currentTarget.value)}
-        />
-        <button className={"btn btn-primary"} onClick={() => {}}>
-          Join Room
+      <input
+        type="text"
+        className={"form-control"}
+        value={roomCode}
+        placeholder={"Enter Room Code"}
+        onInput={(e) => setRoomCode(e.currentTarget.value)}
+      />
+
+      <div className={"container-md d-flex flex-column flex-md-row justify-content-center mt-3"}>
+        <button
+          className={
+            "btn btn-primary"
+          }
+          onClick={() => {
+          }}
+        >
+          Join
+        </button>
+        <span className={"mx-auto my-2"}>or</span>
+        <button
+          className={
+            "btn btn-secondary"
+          }
+          onClick={() => {
+          }}
+        >
+          Create
         </button>
       </div>
-
-      <h3 className={"text-center my-4"}>or</h3>
-      <button
-        className={"btn btn-success btn-lg d-flex lh-1 align-items-center mx-auto"}
-        onClick={() => {}}
-      >
-        <BsPlus size={25} /> Create Room
-      </button>
     </div>
   );
 }
