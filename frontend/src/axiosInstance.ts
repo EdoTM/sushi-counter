7ff006b5088013a7aba4f664b98b75f7f3f45773
edotm <edokyactus@gmail.com>
2024-02-08 14:48:04 +0100

    fix: add credentials to axios

--- frontend/src/axiosInstance.ts
+++ frontend/src/axiosInstance.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:8080',
   timeout: 1000,
+  withCredentials: true,
 });
 
 export default axiosInstance;
