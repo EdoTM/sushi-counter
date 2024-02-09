7142cd4a1b3c082d2f5d9bd06782a487bf3c3039
edotm <edokyactus@gmail.com>
2024-02-09 20:33:32 +0100

    fix: use navigate instead of window href modification

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
-import {createRoom, joinRoom} from "../api.ts";
+import { createRoom, joinRoom } from "../api.ts";
+import { useNavigate } from "react-router-dom";
 
 function HomePage() {
   const [roomName, setRoomName] = useState<string>("");
+  const navigate = useNavigate();
 
   return (
     <div className={"container-fluid pt-5"}>
@@ -16,26 +18,27 @@ function HomePage() {
       />
 
       <div
-        className={
-          "d-flex flex-column flex-sm-row justify-content-center mt-3"
-        }
+        className={"d-flex flex-column flex-sm-row justify-content-center mt-3"}
       >
-        <button className={"btn btn-primary"} onClick={async () => {
-          const res = await createRoom(roomName);
-          if (res.status === 200) {
-            const joinRes = await joinRoom(roomName);
-            if (joinRes.status === 200) {
-              window.location.href = `/room`;
-            } else {
-              alert("Error joining room");
+        <button
+          className={"btn btn-primary"}
+          onClick={async () => {
+            const res = await createRoom(roomName);
+            if (res.status === 200) {
+              const joinRes = await joinRoom(roomName);
+              if (joinRes.status === 200) {
+                navigate(`/room`);
+              } else {
+                alert("Error joining room");
+                return;
+              }
+            } else if (res.status !== 201) {
+              alert("Error creating room");
               return;
             }
-          } else if (res.status !== 201) {
-            alert("Error creating room");
-            return;
-          }
-          window.location.href = `/room`;
-        }}>
+            window.location.href = `/room`;
+          }}
+        >
           Create or join
         </button>
       </div>
