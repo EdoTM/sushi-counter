f2865c8e368a65c6a44d6f4dea233020d6636f27
edotm <edokyactus@gmail.com>
2024-02-09 20:12:29 +0100

    fix: add basename to react router

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -2,17 +2,21 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage.tsx";
 import RoomPage from "./pages/RoomPage";
 
+const BASE_URL = import.meta.env.VITE_HTML_BASE_URL
+
 export function App() {
   const router = createBrowserRouter([
     {
-      path: "/",
+      path: `/`,
       element: <HomePage />,
     },
     {
-      path: "/room",
+      path: `/room`,
       element: <RoomPage />,
     },
-  ]);
+  ], {
+    basename: BASE_URL
+  });
 
   return (
     <div className={"center-window"}>
