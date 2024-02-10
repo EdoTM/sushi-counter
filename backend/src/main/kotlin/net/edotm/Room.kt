9253fe04478190e34150d76a41ee7113e579573a
edotm <edokyactus@gmail.com>
2024-02-10 15:06:15 +0100

    refactor: [wip] rooms handle their users and orders

--- backend/src/main/kotlin/net/edotm/Room.kt
+++ backend/src/main/kotlin/net/edotm/Room.kt
@@ -1,13 +1,34 @@
 package net.edotm
 
+import java.util.concurrent.ConcurrentHashMap
+
 class Room(
     val name: String,
-    val users: MutableSet<UserData> = mutableSetOf(),
+    val users: ConcurrentHashMap<UserData, HashSet<Order>> = ConcurrentHashMap(),
     val expiration: Long = 3_600_000 * 3,
 ) {
 
-    fun aggregateAndGetOrders(): Map<String, Int> {
-        val orders = users.flatMap { it.orders }
-        return orders.groupingBy { it.item }.fold(0) { acc, order -> acc + order.quantity }
+    fun aggregateAndGetOrders(): List<Order> {
+        return users.values
+            .flatten()
+            .groupingBy { it.item }
+            .fold(0) { acc, order -> acc + order.quantity }
+            .map { Order(it.key, it.value) }
+    }
+
+    fun addUser(user: UserData) {
+        users[user] = hashSetOf()
+    }
+
+    fun addUserOrder(user: UserData, order: Order) {
+        users[user]?.add(order)
+    }
+
+    fun removeUser(user: UserData) {
+        users.remove(user)
+    }
+
+    fun removeUserOrder(user: UserData, order: Order) {
+        users[user]?.remove(order)
     }
 }
